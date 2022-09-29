4b83a0105bedbebb4c2bff51791f315348e8b387
tuliotfb <tuliotfb@gmai.com>
2022-09-29 10:03:25 -0300

    Corrigindo comando openssl para nova atualização

--- iac1.sh
+++ iac1.sh
@@ -15,17 +15,17 @@ groupadd GRP_VEN
 groupadd GRP_SEC
 
 echo "Criando usuários..."
-useradd carlos -c "Carlos Pestana" -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_ADM
-useradd maria -c "Maria Ferreira" -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_ADM
-useradd joao -c "João Silva" -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_ADM
+useradd carlos -c "Carlos Pestana" -m -s /bin/bash -p $(openssl passwd senha123) -G GRP_ADM
+useradd maria -c "Maria Ferreira" -m -s /bin/bash -p $(openssl passwd senha123) -G GRP_ADM
+useradd joao -c "João Silva" -m -s /bin/bash -p $(openssl passwd senha123) -G GRP_ADM
 
-useradd debora -c "Débora Sousa" -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_VEN
-useradd sebastiana -c "Sebastiana Pereira" -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_VEN
-useradd robert -c "Roberto Carlos" -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_VEN
+useradd debora -c "Débora Sousa" -m -s /bin/bash -p $(openssl passwd senha123) -G GRP_VEN
+useradd sebastiana -c "Sebastiana Pereira" -m -s /bin/bash -p $(openssl passwd senha123) -G GRP_VEN
+useradd robert -c "Roberto Carlos" -m -s /bin/bash -p $(openssl passwd senha123) -G GRP_VEN
 
-useradd josefina -c "Josefina Costa" -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_SEC
-useradd Amanda -c "Amanda Sousa" -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_SEC
-useradd rogerio -c "Rogerio Feitosa" -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_SEC
+useradd josefina -c "Josefina Costa" -m -s /bin/bash -p $(openssl passwd senha123) -G GRP_SEC
+useradd Amanda -c "Amanda Sousa" -m -s /bin/bash -p $(openssl passwd senha123) -G GRP_SEC
+useradd rogerio -c "Rogerio Feitosa" -m -s /bin/bash -p $(openssl passwd senha123) -G GRP_SEC
 
 echo "Especificando permissões de acesso aos diretórios..."
 chown root:GRP_ADM /adm
